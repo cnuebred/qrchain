5ef59a4db303df63147da151afd71ab63a7f025b
cnuebred <jonatann01ew@gmai.com>
2022-04-11 01:18:25 +0200

    add project config files

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2020': true,
+  },
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'no-unused-vars': 0,
+    'require-jsdoc': 0,
+    'max-len': ['error', {'code': 100, 'tabWidth': 4}],
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'linebreak-style': 0,
+    'indent': 0,
+    'eqeqeq': 0,
+  },
+}
