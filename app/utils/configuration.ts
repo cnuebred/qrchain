5c74da0a685cd74764e17367628a4e3bdbd40e94
cnuebred <jonatann01ew@gmai.com>
2022-04-12 01:56:22 +0200

    add configuration core

---
+++ app/utils/configuration.ts
@@ -0,0 +1,23 @@
+type Configuration = {
+    server?: {
+        host: string
+        port: number
+        api: string
+    }
+}
+
+export const _config: Configuration = {}
+
+const pushToConfig = (_package: { [index: string]: string }) => {
+    Object.entries(_package).forEach(([key, value]) => {
+        _config[key] = value
+    })
+}
+export const setupConfig = async () => {
+    const mode = process.env.MODE
+    if (mode == 'dev') {
+        pushToConfig(require('../config/config.dev.js'))
+    } else if (mode == 'prod') {
+        pushToConfig(require('../config/config.prod.js'))
+    }
+}
