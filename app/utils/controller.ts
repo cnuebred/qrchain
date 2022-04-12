26d74a01005f09a7f09883beb334f72ccecbb5fe
cnuebred <jonatann01ew@gmai.com>
2022-04-13 01:17:11 +0200

    update decorator for methods
    ability to put require parts of metadata

--- app/utils/controller.ts
+++ app/utils/controller.ts
@@ -1,9 +1,12 @@
+import { res } from './responses'
+import { ExpressArgs, MetaRequires } from './utils'
+
 const restMethod = (
     target: Object,
     name: string,
     f: PropertyDescriptor,
-    path: string,
-    methodType: 'get' | 'post' | 'delete' | 'patch' | 'put'
+    methodType: 'get' | 'post' | 'delete' | 'patch' | 'put',
+    path: string
 ) => {
     const set = target.constructor.prototype
     const setPrefix = `set_${name}`
@@ -14,11 +17,44 @@ const restMethod = (
             method: [methodType],
             worker: f.value,
         }
-    return
 }
 const restWrapper = (method: 'get' | 'post' | 'delete' | 'patch' | 'put') => {
-    return (path: string = '') => {
-        return (target, name, f) => restMethod(target, name, f, path, method)
+    return (path: string = '', requires?: MetaRequires) => {
+        return (target: Object, name: string, f: PropertyDescriptor) => {
+            const orginalMethod = f.value
+            console.log('spoko_', name)
+            f.value = (args: ExpressArgs) => {
+                const notContains = {}
+                if (requires)
+                    Object.entries(requires).forEach(([meta, properties]) => {
+                        notContains[meta] = []
+                        if (!args.req[meta]) return
+                        const keys = Object.keys(args.req[meta])
+                        properties.forEach((item) => {
+                            if (!item) return
+                            if (!keys.includes(item))
+                                notContains[meta].push(item)
+                        })
+                    })
+                const notContainsCondition =
+                    Object.values(notContains)
+                        .flat()
+                        .filter((item) => !!item).length != 0
+
+                if (notContainsCondition)
+                    return args.res
+                        .status(401)
+                        .send(
+                            res()
+                                .err()
+                                .msg('Missing require data')
+                                .data(notContains).response
+                        )
+
+                return orginalMethod(args)
+            }
+            restMethod(target, name, f, method, path)
+        }
     }
 }
 
