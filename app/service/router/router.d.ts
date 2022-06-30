42f51a6a04279113946745181b003acc40d350a2
cnuebred <jonatann01ew@gmai.com>
2022-06-30 11:20:20 +0200

    update router service module
    update definitions, fix router core, add new components to router module

---
+++ app/service/router/router.d.ts
@@ -0,0 +1,16 @@
+import { MetaOptions } from '../../utils/utils'
+
+export type Leaf = {
+    name: string
+    rootName: string
+    method: string[]
+    rootPath: string
+    path: string
+    status: boolean
+    options: MetaOptions
+}
+export interface ControllerType {
+    __root__: { name: string; parent: string }
+    __init__: () => void
+    __name__: string
+}
---
+++ app/service/router/router.module.ts
@@ -0,0 +1,5 @@
+import { Inspector } from '../../components/inspector/inspector.component'
+import { Qrcode } from '../../components/qrcode/qrcode.component'
+import { User } from '../../components/user/user.component'
+
+export const components = () => [Qrcode, User, Inspector]
