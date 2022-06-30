091934a6f48f7c68a677b8cb99ce987f5ac5e602
cnuebred <jonatann01ew@gmai.com>
2022-06-30 11:29:19 +0200

    update config
    update dataset and sample file

--- app/config/config.d.ts
+++ app/config/config.d.ts
@@ -1,17 +1,26 @@
 export type Configuration = {
-    server?: {
+    server: {
         host: string
         port: number
         api: string
     }
-    db?: {
+    db: {
         hostname: string
         password: string
         database: string
         user: string
-    }
-    utils?: {
+    },
+    ws: {
+        host: string,
+        port: number
+    },
+    utils: {
         auth_algo: string
         sec_key: string
+    }
+    auth: {
+        db: {
+            verify_session: string[]
+        }
     }
 }
--- app/config/config.dev.ts
+++ app/config/config.dev.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export = {
     server: {
         host: 'localhost',
         port: 8080,
@@ -10,8 +10,17 @@ module.exports = {
         user: 'postgres',
         password: 'admin',
     },
+    ws: {
+        host: 'localhost',
+        port: 8008
+    },
     utils: {
         auth_algo: 'aes-256-cbc',
         sec_key: 'b98ec4700039a24fca73fcb6251237be'
+    },
+    auth: {
+        db: {
+            verify_session: ['token_hash', 'member_session', 'member_hash']
+        }
     }
 }
